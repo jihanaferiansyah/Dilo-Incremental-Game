a6117b5e54bdfa108975d5bb882df748ee04bb54
jihanaferiansyah <jihanaferian@gmail.com>
2021-09-12 16:22:25 +0700

    Dilo Course

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
